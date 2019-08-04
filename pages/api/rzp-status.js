82ec756a529ab329fa403a67ee56d6ca3e618ce1
Vinay Puppal <me@vinaypuppal.com>
2019-08-04 09:22:00 +0530

    fix status related issues

--- pages/api/rzp-status.js
+++ pages/api/rzp-status.js
@@ -1,31 +1,23 @@
 const crypto = require('crypto');
 import { donationsBase } from '../../services/airtable';
 
-const rzpCredentials = {
-  key_id: `rzp_live_NNM9DbPxGLCoNV`,
-  key_secret: `b3LcIImih14EGfELhgUZGWom`,
-};
-
-const rzpTestCredentials = {
-  key_id: `rzp_test_FEPb74blpXOIIf`,
-  key_secret: `fguOShIqgV49u6ewLZBf29Q4`,
-};
+const RZP_SECRET = process.env.NODE_ENV === 'development' ? process.env.RZP_TEST_SECRET : process.env.RZP_LIVE_SECRET;
 
 export default async function handler(req, res) {
   try {
     const { razorpay_order_id, razorpay_payment_id, razorpay_signature, status } = req.body;
     if (status !== 'captured') {
-      updateStateInAirtable(req.body);
+      await updateStateInAirtable(req.body);
       return res.send({ success: true });
     }
 
     const generatedSignature = crypto
-      .createHmac('SHA256', rzpTestCredentials.key_secret)
+      .createHmac('SHA256', RZP_SECRET)
       .update(razorpay_order_id + '|' + razorpay_payment_id)
       .digest('hex');
 
     if (razorpay_signature !== generatedSignature) {
-      updateStateInAirtable({ ...req.body, status: 'failed' });
+      await updateStateInAirtable({ ...req.body, status: 'failed' });
       return res.send({ success: true });
     }
 
@@ -44,6 +36,9 @@ async function updateStateInAirtable({ razorpay_order_id, razorpay_payment_id, s
       view: 'table',
     })
     .firstPage();
+  if (!record.id) {
+    return;
+  }
   return donationsBase.update(record.id, {
     Status: status,
     'Payment Id': razorpay_payment_id || '',
