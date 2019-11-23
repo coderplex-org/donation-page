91a595b0d3aa61ef01cc3fd12cd21447ece55f2b
vinaypuppal <me@vinaypuppal.com>
2019-11-23 21:15:34 +0530

    move everything into `src` and `public` folders

--- src/services/airtable.js
+++ src/services/airtable.js
@@ -3,7 +3,7 @@ import Airtable from 'airtable';
 const airtableCredentials = {
   key: process.env.AIRTABLE_KEY,
   baseId: process.env.AIRTABLE_BASE_ID,
-  donationsTableName: process.env.AIRTABLE_TABLE_NAME,
+  donationsTableName: process.env.AIRTABLE__DONATIONS_TABLE_NAME,
   campaignsTableName: process.env.AIRTABLE__CAMPAIGN_TABLE_NAME,
   fundingsTableName: process.env.AIRTABLE__FUNDING_TABLE_NAME,
 };
